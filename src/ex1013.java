084a3dcffee2660087ac6769b0635cb3cfc0b024
valeskamdev <valeskkitty@gmail.com>
2022-11-26 19:32:57 -0300

    O maior

---
+++ src/ex1013.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ex1013 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        int maiorAB = (a + b + Math.abs(a - b)) / 2;
+        int maiorABC = (maiorAB + c + Math.abs(maiorAB - c)) / 2;
+
+        System.out.println(maiorABC + " eh o maior");
+
+        scanner.close();
+    }
+}
