c9d97aa7b9e3fc7420aaf696ecc82fb573addcbc
valeskamdev <107771511+valeskamdev@users.noreply.github.com>
2023-01-10 13:26:47 -0300

    Numeros pares

---
+++ src/ex2039.java
@@ -0,0 +1,11 @@
+
+public class ex2039 {
+    public static void main(String[] args) {
+
+       for (int i = 1; i <= 100; i++) {
+           if (i % 2 == 0) {
+               System.out.println(i);
+           }
+       }
+    }
+}
